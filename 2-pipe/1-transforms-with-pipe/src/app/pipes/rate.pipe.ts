4e4024811743ffef6b5e29daf483d9d8b066e282
ArtChr <artchrr@gmail.com>
2021-01-11 18:58:54 +0200

    module-2 solution

--- 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
@@ -6,6 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 // @ts-ignore
 export class RatePipe implements PipeTransform {
   public transform(value: number): number | undefined {
-    return 0;
+    const integerValue = Math.floor(value);
+    const fractional = value - integerValue;
+
+    if (fractional < 0.25) {
+      return integerValue;
+    } else if (fractional < 0.75 && fractional >= 0.25) {
+      return integerValue + 0.5;
+    }
+
+    return Math.ceil(value);
   }
 }
--- 2-pipe/1-transforms-with-pipe/src/app/pipes/review.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/review.pipe.ts
@@ -5,7 +5,30 @@ import { PipeTransform, Pipe } from '@angular/core';
 })
 // @ts-ignore
 export class ReviewPipe implements PipeTransform {
+
   public transform(countOfReviews: number | undefined): string {
-    return '';
+
+    if (!countOfReviews) {
+      return 'Нет отзывов';
+    }
+
+    const countOfReviewsArray = countOfReviews.toString().split('');
+    const lastChars = +(countOfReviewsArray[countOfReviewsArray.length - 1]);
+    const lastTwoChars = +(countOfReviewsArray[countOfReviewsArray.length - 2] + countOfReviewsArray[countOfReviewsArray.length - 1]);
+
+    if ([11, 12, 13, 14].includes(lastTwoChars)) {
+      return `${ countOfReviews } отзывов`;
+    }
+
+    if (lastChars === 1) {
+      return `${ countOfReviews } отзыв`;
+    }
+
+    if ([2, 3, 4].includes(lastChars)) {
+      return `${ countOfReviews } отзыва`;
+    }
+
+    return `${ countOfReviews } отзывов`;
   }
+
 }
