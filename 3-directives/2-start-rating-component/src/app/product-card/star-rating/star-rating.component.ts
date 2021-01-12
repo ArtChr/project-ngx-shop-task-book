0cb4e873b60613985decc0def06d5e587b7a445f
ArtChr <artchrr@gmail.com>
2021-01-12 20:03:53 +0200

    module-3-2 solution

--- 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
+++ 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-star-rating',
   templateUrl: './star-rating.component.html',
 })
 export class StarRatingComponent {
+
+  @Input()
+  public feedbackRate = 0;
+
+  public stars: number[] = [0, 1, 2, 3, 4];
+
+  public highlight(idx: number): boolean {
+    return idx < Math.round(this.feedbackRate);
+  }
 
 }
