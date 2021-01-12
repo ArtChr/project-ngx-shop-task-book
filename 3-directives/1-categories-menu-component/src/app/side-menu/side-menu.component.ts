f3abab6fe922f21e7b9b768c8586f94c95161a5e
ArtChr <artchrr@gmail.com>
2021-01-12 17:37:23 +0200

    module-3-1 solution

--- 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
+++ 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
@@ -1,7 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICategory, ISubCategory } from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-side-menu',
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+
+  @Input()
+  public categories: ICategory[] = [];
+
+  @Output()
+  public categoryChange: EventEmitter<string> = new EventEmitter<string>();
+
+  public currentName: string = null;
+
+  public redirectTo(subCategoryId: string): void {
+    this.categoryChange.emit(subCategoryId);
+  }
+
+  public hover(subCategoryId: string): void {
+    this.currentName = subCategoryId;
+  }
+
+  public mouseLeave(): void {
+    this.currentName = null;
+  }
+
+}
