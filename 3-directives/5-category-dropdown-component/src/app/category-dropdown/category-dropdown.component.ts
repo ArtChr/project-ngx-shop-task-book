587ec2d2d4c86fa88466aeaa9ad78f6a9ac59c16
ArtChr <artchrr@gmail.com>
2021-01-15 22:21:17 +0200

    module-3-5 solution

--- 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
+++ 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICategory, ISubCategory } from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-category-dropdown',
   templateUrl: './category-dropdown.component.html',
 })
-export class CategoryDropdownComponent {}
+export class CategoryDropdownComponent {
+
+  @Input()
+  public categories: ICategory[] = [];
+
+  @Output()
+  public subCategorySelectEvent: EventEmitter<string> = new EventEmitter<string>();
+
+  public currentCategory = '';
+  public currentIndex = null;
+
+  public subCategorySelect(subCat: ISubCategory): void {
+    this.currentCategory = subCat._id;
+    this.subCategorySelectEvent.emit(subCat.name);
+  }
+
+  public showSubCategories(idx: number): void {
+    this.currentIndex = idx;
+  }
+
+}
